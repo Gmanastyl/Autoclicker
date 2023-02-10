d4ac6d0a94097c276de9eaeefe19b6267bdeeadf
Schofield, stuart <546wine@gmail.com>
2023-02-09 22:40:36 -0700

    Got the arrows not crossing over working properly

--- Autoclicker-v1/Views/StuMainWindow.axaml.cs
+++ Autoclicker-v1/Views/StuMainWindow.axaml.cs
@@ -1,8 +1,12 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Chrome;
 using Avalonia.Input;
 using Avalonia.Interactivity;
+using Avalonia.Media;
+using Avalonia.Styling;
 using Avalonia.Themes.Default;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace Autoclicker_v1.Views;
 
@@ -19,8 +23,10 @@ public partial class StuMainWindow : Window
     // The image that was clicked on
     private Image? selectedImage;
 
+    private Image converseImage;
+
     // Flag indicating if the arrows on the slider should be able to cross each other
-    private bool arrowsShouldCross = false;
+    private bool shouldArrowsCross = false;
 
     // The minimum the left click arrows can slide
     private double leftClickArrowLeftMarginMin = 0;
@@ -65,10 +71,16 @@ public partial class StuMainWindow : Window
     private void LeftClick_SliderImage_PointerPressed(object sender, PointerPressedEventArgs e)
     {
         // Check to see if the object that was clicked on is actually an image
-                if (sender is Image)
+        if (sender is Image)
         {
             // Set the selected image to the image pressed on
             selectedImage = sender as Image;
+
+            if (selectedImage == leftClickLeftArrow)
+                converseImage = leftClickRightArrow;
+
+            else
+                converseImage = leftClickLeftArrow;
 
             // Set the Flag to indicate an image has been pressed on
             imagePressed = true;
@@ -112,25 +124,84 @@ public partial class StuMainWindow : Window
             var distance = mousePosition.X - pointerPressedPosition.X;
 
             // Calculate the new left margin for the image
-            var newLeftMarginValue = pointerPressedPosition.X + distance;
+            var newSelectedImageMarginValue = pointerPressedPosition.X + distance;
 
-            // If the new left margin is greater than the left margin maximum minus
-            // the width of the image, then set the new left margin to the max left margin
-            if (newLeftMarginValue > leftClickArrowLeftMarginMax - selectedImage.Width) 
-                newLeftMarginValue = leftClickSlider.Width - selectedImage.Width;
+            double newConverseImageMarginValue = converseImage.Margin.Left;
 
-            // If the new left margin is less than the minimum left margin then set the
-            // new left margin to the minimum left margin
-            if (newLeftMarginValue < leftClickArrowLeftMarginMin) newLeftMarginValue = leftClickArrowLeftMarginMin;
+            // If the selected image is a left arrow...
+            if (selectedImage == leftClickLeftArrow)
+            {
+                // If the new margin for the selected image is less than the minimum...
+                if (newSelectedImageMarginValue < leftClickArrowLeftMarginMin)
+                    newSelectedImageMarginValue = leftClickArrowLeftMarginMin;
 
-            // Update the left margin of the image with the new left margin value
-            selectedImage.Margin = new Thickness(
-                newLeftMarginValue,
-                selectedImage.Margin.Top,
-                selectedImage.Margin.Right,
-                selectedImage.Margin.Bottom);
+                // If the new margin for the selected image is greater than the maximum
+                if (newSelectedImageMarginValue > leftClickArrowLeftMarginMax - selectedImage.Width - converseImage.Width)
+                {
+                    // Set the selected image margin to the maximum
+                    newSelectedImageMarginValue = leftClickArrowLeftMarginMax - selectedImage.Width - converseImage.Width;
+
+                    newConverseImageMarginValue = leftClickArrowLeftMarginMax - converseImage.Width;
+                }
+
+                if (newSelectedImageMarginValue + selectedImage.Width > converseImage.Margin.Left)
+                    newConverseImageMarginValue = newSelectedImageMarginValue + selectedImage.Width;
+
+                converseImage.Margin = new Thickness(
+                       newConverseImageMarginValue,
+                       selectedImage.Margin.Top,
+                       selectedImage.Margin.Right,
+                       selectedImage.Margin.Bottom);
+
+
+                selectedImage.Margin = new Thickness(
+                       newSelectedImageMarginValue,
+                       selectedImage.Margin.Top,
+                       selectedImage.Margin.Right,
+                       selectedImage.Margin.Bottom);
+            }
+
+
+
+            // If the selected image is a right arrow...
+            if (selectedImage == leftClickRightArrow)
+            {
+                // If the new margin for the selected image is greater than the maximum...
+                if (newSelectedImageMarginValue > leftClickArrowLeftMarginMax - selectedImage.Width)
+                    newSelectedImageMarginValue = leftClickArrowLeftMarginMax - selectedImage.Width;
+
+                // If the new margin for the selected image is less than the minimum
+                if (newSelectedImageMarginValue < leftClickArrowLeftMarginMin + converseImage.Width)
+                {
+                    // Set the selected image margin to the minimum
+                    newSelectedImageMarginValue = leftClickArrowLeftMarginMin + converseImage.Width;
+
+                    newConverseImageMarginValue = leftClickArrowLeftMarginMin;
+                }
+
+                // If the the right arrow margin is less than the left image margin
+                if (newSelectedImageMarginValue < newConverseImageMarginValue + converseImage.Width)
+                    newConverseImageMarginValue = newSelectedImageMarginValue - converseImage.Width;
+
+                converseImage.Margin = new Thickness(
+                       newConverseImageMarginValue,
+                       selectedImage.Margin.Top,
+                       selectedImage.Margin.Right,
+                       selectedImage.Margin.Bottom);
+
+
+                selectedImage.Margin = new Thickness(
+                       newSelectedImageMarginValue,
+                       selectedImage.Margin.Top,
+                       selectedImage.Margin.Right,
+                       selectedImage.Margin.Bottom);
+            }
+
         }
+
     }
+
+
 
 
     /// <summary>
@@ -155,10 +226,16 @@ public partial class StuMainWindow : Window
             // Create an event to call the method when the pointer moves
             rightClickSlider.PointerMoved += RightClick_SliderImage_PointerMoved;
 
-        }
-      
+            if (selectedImage == rightClickLeftArrow)
+                converseImage = rightClickRightArrow;
 
-        
+            else
+                converseImage = rightClickLeftArrow;
+
+        }
+
+
+
     }
     /// <summary>
     /// This method is called when the pointer is released
@@ -180,7 +257,6 @@ public partial class StuMainWindow : Window
     /// <param name="e">The pointer args</param>
     private void RightClick_SliderImage_PointerMoved(object sender, PointerEventArgs e)
     {
-
         // If we have already clicked on an image...
         if (imagePressed)
         {
@@ -191,25 +267,145 @@ public partial class StuMainWindow : Window
             var distance = mousePosition.X - pointerPressedPosition.X;
 
             // Calculate the new left margin for the image
-            var newLeftMarginValue = pointerPressedPosition.X + distance;
+            var newSelectedImageMarginValue = pointerPressedPosition.X + distance;
 
-            // If the new left margin is greater than the left margin maximum minus
-            // the width of the image, then set the new left margin to the max left margin
-            if (newLeftMarginValue > rightClickArrowLeftMarginMax - selectedImage.Width)
-                newLeftMarginValue = rightClickSlider.Width - selectedImage.Width;
+            double newConverseImageMarginValue = converseImage.Margin.Left;
 
-            // If the new left margin is less than the minimum left margin then set the
-            // new left margin to the minimum left margin
-            if (newLeftMarginValue < rightClickArrowLeftMarginMin) newLeftMarginValue = rightClickArrowLeftMarginMin;
+            // If the selected image is a left arrow...
+            if (selectedImage == rightClickLeftArrow)
+            {
+                // If the new margin for the selected image is less than the minimum...
+                if (newSelectedImageMarginValue < rightClickArrowLeftMarginMin)
+                    newSelectedImageMarginValue = rightClickArrowLeftMarginMin;
 
-            // Update the left margin of the image with the new left margin value
-            selectedImage.Margin = new Thickness(
-                newLeftMarginValue,
-                selectedImage.Margin.Top,
-                selectedImage.Margin.Right,
-                selectedImage.Margin.Bottom);
+                // If the new margin for the selected image is greater than the maximum
+                if (newSelectedImageMarginValue > rightClickArrowLeftMarginMax - selectedImage.Width - converseImage.Width)
+                {
+                    // Set the selected image margin to the maximum
+                    newSelectedImageMarginValue = rightClickArrowLeftMarginMax - selectedImage.Width - converseImage.Width;
+
+                    newConverseImageMarginValue = rightClickArrowLeftMarginMax - converseImage.Width;
+                }
+
+                if (newSelectedImageMarginValue + selectedImage.Width > converseImage.Margin.Left)
+                    newConverseImageMarginValue = newSelectedImageMarginValue + selectedImage.Width;
+
+                converseImage.Margin = new Thickness(
+                       newConverseImageMarginValue,
+                       selectedImage.Margin.Top,
+                       selectedImage.Margin.Right,
+                       selectedImage.Margin.Bottom);
+
+
+                selectedImage.Margin = new Thickness(
+                       newSelectedImageMarginValue,
+                       selectedImage.Margin.Top,
+                       selectedImage.Margin.Right,
+                       selectedImage.Margin.Bottom);
+            }
+
+
+
+            // If the selected image is a right arrow...
+            if (selectedImage == rightClickRightArrow)
+            {
+                // If the new margin for the selected image is greater than the maximum...
+                if (newSelectedImageMarginValue > rightClickArrowLeftMarginMax - selectedImage.Width)
+                    newSelectedImageMarginValue = rightClickArrowLeftMarginMax - selectedImage.Width;
+
+                // If the new margin for the selected image is less than the minimum
+                if (newSelectedImageMarginValue < rightClickArrowLeftMarginMin + converseImage.Width)
+                {
+                    // Set the selected image margin to the minimum
+                    newSelectedImageMarginValue = rightClickArrowLeftMarginMin + converseImage.Width;
+
+                    newConverseImageMarginValue = rightClickArrowLeftMarginMin;
+                }
+
+                // If the the right arrow margin is less than the left image margin
+                if (newSelectedImageMarginValue < newConverseImageMarginValue + converseImage.Width)
+                    newConverseImageMarginValue = newSelectedImageMarginValue - converseImage.Width;
+
+                converseImage.Margin = new Thickness(
+                       newConverseImageMarginValue,
+                       selectedImage.Margin.Top,
+                       selectedImage.Margin.Right,
+                       selectedImage.Margin.Bottom);
+
+
+                selectedImage.Margin = new Thickness(
+                       newSelectedImageMarginValue,
+                       selectedImage.Margin.Top,
+                       selectedImage.Margin.Right,
+                       selectedImage.Margin.Bottom);
+            }
 
         }
+
+        //blah ablha
+
+        //// If we have already clicked on an image...
+        //if (imagePressed)
+        //{
+        //    // Get the pointer position 
+        //    Point mousePosition = e.GetPosition(rightClickSlider);
+
+        //    // Calculate the distance the pointer has moved from the original pointer position
+        //    var distance = mousePosition.X - pointerPressedPosition.X;
+
+        //    // Calculate the new left margin for the image
+        //    var newLeftMarginValue = pointerPressedPosition.X + distance;
+
+        //    // If the new left margin is greater than the left margin maximum minus
+        //    // the width of the image, then set the new left margin to the max left margin
+        //    if (newLeftMarginValue > rightClickArrowLeftMarginMax - selectedImage.Width)
+        //        newLeftMarginValue = rightClickSlider.Width - selectedImage.Width;
+
+        //    // If the new left margin is less than the minimum left margin then set the
+        //    // new left margin to the minimum left margin
+        //    if (newLeftMarginValue < rightClickArrowLeftMarginMin) newLeftMarginValue = rightClickArrowLeftMarginMin;
+
+        //    // If the left arrow is the one sliding and it runs into the right arrow,
+        //    //    move the right arrow also
+        //    if (selectedImage.Name == "rightClickLeftArrow"
+        //        && newLeftMarginValue > rightClickRightArrow.Margin.Left - selectedImage.Width
+        //        && !arrowsShouldCross)
+        //    {
+        //        var leftMargin = newLeftMarginValue;
+
+        //        //if (leftMargin + rightClickLeftArrow.Width > rightClickArrowLeftMarginMax)
+        //        //    leftMargin = rightClickArrowLeftMarginMax;
+
+        //        rightClickRightArrow.Margin =
+        //            new Thickness(
+        //                leftMargin + selectedImage.Width,
+        //                selectedImage.Margin.Top,
+        //                selectedImage.Margin.Right,
+        //                selectedImage.Margin.Bottom);
+        //    }
+
+        //    // If the right arrow is the one sliding and it runs into the left arrow,
+        //    //    move the left arrow also
+        //    if (selectedImage.Name == "rightClickRightArrow"
+        //        && newLeftMarginValue - rightClickRightArrow.Width < rightClickLeftArrow.Margin.Left
+        //        && !arrowsShouldCross)
+        //    {
+        //        rightClickLeftArrow.Margin =
+        //            new Thickness(
+        //                newLeftMarginValue - rightClickLeftArrow.Width,
+        //                selectedImage.Margin.Top,
+        //                selectedImage.Margin.Right,
+        //                selectedImage.Margin.Bottom);
+        //    }
+
+        //    // Update the left margin of the image with the new left margin value
+        //    selectedImage.Margin = new Thickness(
+        //        newLeftMarginValue,
+        //        selectedImage.Margin.Top,
+        //        selectedImage.Margin.Right,
+        //        selectedImage.Margin.Bottom);
+
+        //}
     }
 
     #endregion EndRegion - Private Methods
@@ -247,3 +443,4 @@ public partial class StuMainWindow : Window
         }
     }
 }
+
