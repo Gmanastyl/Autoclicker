e8a450b213949b7c79d1473976e049f8b1021fe9
Schofield, stuart <546wine@gmail.com>
2023-02-09 17:37:37 -0700

    Fixed 900x600 problem and commented and cleaned up Window.axaml.cs style... it still needs more

--- Autoclicker-v1/Views/StuMainWindow.axaml.cs
+++ Autoclicker-v1/Views/StuMainWindow.axaml.cs
@@ -2,16 +2,19 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Interactivity;
+using Avalonia.Themes.Default;
 
 namespace Autoclicker_v1.Views;
 
 public partial class StuMainWindow : Window
 {
+    #region Private Members
+
     // Flag indicating if when the mouse button was pressed, it was on an image
     private bool imagePressed;
 
     // The position on the canvas where the mouse button was clicked
-    private Point clickPosition;
+    private Point pointerPressedPosition;
 
     // The image that was clicked on
     private Image? selectedImage;
@@ -19,96 +22,161 @@ public partial class StuMainWindow : Window
     // Flag indicating if the arrows on the slider should be able to cross each other
     private bool arrowsShouldCross = false;
 
+    // The minimum the left click arrows can slide
+    private double leftClickArrowLeftMarginMin = 0;
 
-    // 
-    private double leftClickRightArrowLeftMarginMin = 0;
-    private double leftClickRightArrowMarginLeftMax = 145; 
+    // The maximum the left click arrows can slide
+    private double leftClickArrowLeftMarginMax = 150;
 
-    private double rightClickLeftArrowLeftMarginMin = 0;
-    private double rightClickRightArrowMarginLeftMax = 145;
+    // The minimum the left click arrows can slide
+    private double rightClickArrowLeftMarginMin = 0;
+
+    // The maximum the left click arrows can slide
+    private double rightClickArrowLeftMarginMax = 150;
+
+    // The left margin value when the image was pressed on
+    private double selectedImageOriginalLeftMargin;
 
 
+    #endregion EndRegion - Private Members
+
+
+    #region Constructors
+
+    /// <summary>
+    /// Default Constructor
+    /// </summary>
     public StuMainWindow()
     {
         InitializeComponent();
 
     }
 
-    private void LeftSliderImage_MouseLeftButtonDown(object sender, PointerPressedEventArgs e)
+    #endregion EndRegion - Constructors
+
+
+    #region Private Methods
+
+    /// <summary>
+    /// This method is called when either of the arrows in the left click slider is clicked
+    /// </summary>
+    /// <param name="sender">The object in the view that was clicked on</param>
+    /// <param name="e">The pointer args</param>
+    private void LeftClick_SliderImage_PointerPressed(object sender, PointerPressedEventArgs e)
     {
-        if (sender is Image)
+        // Check to see if the object that was clicked on is actually an image
+                if (sender is Image)
         {
+            // Set the selected image to the image pressed on
             selectedImage = sender as Image;
 
+            // Set the Flag to indicate an image has been pressed on
             imagePressed = true;
 
-            clickPosition = e.GetPosition(leftClickSlider);
+            // Get the pointer position when the image was pressed on
+            pointerPressedPosition = e.GetPosition(leftClickSlider);
 
-            leftClickSlider.PointerMoved += LeftSliderImage_Moved;
-
-            
+            // Create an event to call the method when the pointer moves
+            leftClickSlider.PointerMoved += LeftClick_SliderImage_PointerMoved;
 
         }
     }
 
-    private void LeftSliderImage_Moved(object sender, PointerEventArgs e)
+    /// <summary>
+    /// This method is called when the pointer is released
+    /// The parameters of the method should be able to be removed
+    /// </summary>
+    /// <param name="sender">Not used</param>
+    /// <param name="e">Not used</param>
+    private void LeftClick_SliderImage_PointerReleased(object sender, PointerReleasedEventArgs e)
     {
+        // Set the image pressed Flag to false
+        imagePressed = false;
+    }
 
+    /// <summary>
+    /// Called when the pointer moves
+    /// This is called multiple times during a slide
+    /// </summary>
+    /// <param name="sender">Not used</param>
+    /// <param name="e">The pointer args</param>
+    private void LeftClick_SliderImage_PointerMoved(object sender, PointerEventArgs e)
+    {
+        // If we have already clicked on an image...
         if (imagePressed)
         {
+            // Get the position of the pointer after it has moved
             var xpos = e.GetPosition(leftClickSlider).X;
-            var srml = selectedImage.Margin.Left;
 
-            var initialPointerPositionLocationInRectangleOffset =
-                e.GetPosition(leftClickSlider).X - selectedImage.Margin.Left;
-
+            // Get the pointer position 
             Point mousePosition = e.GetPosition(leftClickSlider);
 
-            var m = mousePosition.X;
+            // Calculate the distance the pointer has moved from the original pointer position
+            var distance = mousePosition.X - pointerPressedPosition.X;
 
-            var distance = mousePosition.X - clickPosition.X;
+            // Calculate the new left margin for the image
+            var newLeftMarginValue = pointerPressedPosition.X + distance;
 
-            var left = clickPosition.X + distance;
+            // If the new left margin is greater than the left margin maximum minus
+            // the width of the image, then set the new left margin to the max left margin
+            if (newLeftMarginValue > leftClickArrowLeftMarginMax - selectedImage.Width) 
+                newLeftMarginValue = leftClickSlider.Width - selectedImage.Width;
 
-            var x = (double)leftClickSlider.Width;
+            // If the new left margin is less than the minimum left margin then set the
+            // new left margin to the minimum left margin
+            if (newLeftMarginValue < leftClickArrowLeftMarginMin) newLeftMarginValue = leftClickArrowLeftMarginMin;
 
-            if (left > leftClickSlider.Width - selectedImage.Width) left = leftClickSlider.Width - selectedImage.Width;
-
-            if (left < 0) left = 0;
-
-            var leftOffset = left;
-
+            // Update the left margin of the image with the new left margin value
             selectedImage.Margin = new Thickness(
-                left,
+                newLeftMarginValue,
                 selectedImage.Margin.Top,
                 selectedImage.Margin.Right,
                 selectedImage.Margin.Bottom);
         }
-
     }
 
-    private void LeftSliderImage_MouseLeftButtonUp(object sender, PointerReleasedEventArgs e)
+
+    /// <summary>
+    /// This method is called when either of the arrows in the right click slider is clicked
+    /// </summary>
+    /// <param name="sender">The object in the view that was clicked on</param>
+    /// <param name="e">The pointer args</param>
+    private void RightClick_SliderImage_PointerPressed(object sender, PointerPressedEventArgs e)
     {
+        // Check to see if the object that was clicked on is actually an image
+        if (sender is Image)
+        {
+            // Set the selected image to the image pressed on
+            selectedImage = sender as Image;
+
+            // Set the Flag to indicate an image has been pressed on
+            imagePressed = true;
+
+            // Get the pointer position when the image was pressed on
+            pointerPressedPosition = e.GetPosition(rightClickSlider);
+
+            // Create an event to call the method when the pointer moves
+            rightClickSlider.PointerMoved += RightClick_SliderImage_PointerMoved;
+
+        }
+      
+
+        
+    }
+    /// <summary>
+    /// This method is called when the pointer is released
+    /// The parameters of the method should be able to be removed
+    /// </summary>
+    /// <param name="sender">Not used</param>
+    /// <param name="e">Not used</param>
+    private void RightClick_SliderImage_PointerReleased(object sender, PointerReleasedEventArgs e)
+    {
+        // Set the image pressed Flag to false
         imagePressed = false;
     }
 
-    private void RightSliderImage_MouseLeftButtonDown(object sender, PointerPressedEventArgs e)
-    {
-        if (sender is Image)
-        {
 
-            imagePressed = true;
-
-            clickPosition = e.GetPosition(leftClickSlider);
-
-            rightClickSlider.PointerMoved += RightSliderImage_Moved;
-
-            selectedImage = sender as Image;
-
-        }
-    }
-
-    private void RightSliderImage_Moved(object sender, PointerEventArgs e)
+    private void RightClick_SliderImage_PointerMoved(object sender, PointerEventArgs e)
     {
 
         if (imagePressed)
@@ -123,9 +191,9 @@ public partial class StuMainWindow : Window
 
             var m = mousePosition.X;
 
-            var distance = mousePosition.X - clickPosition.X;
+            var distance = mousePosition.X - pointerPressedPosition.X;
 
-            var left = clickPosition.X + distance;
+            var left = pointerPressedPosition.X + distance;
 
             var x = (double)rightClickSlider.Width;
 
@@ -144,10 +212,8 @@ public partial class StuMainWindow : Window
 
     }
 
-    private void RightSliderImage_MouseLeftButtonUp(object sender, PointerReleasedEventArgs e)
-    {
-        imagePressed = false;
-    }
+    #endregion EndRegion - Private Methods
+
 
 
     private void HideButton_Click(object sender, RoutedEventArgs e)
